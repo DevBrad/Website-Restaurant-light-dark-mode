be29b0145ecdbee97aa0836955973f752d14eaba
BradleyMirel <mirelmandrut94@gmail.com>
2021-01-30 11:52:26 +0200

    wip on the main.js file

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 /*=====SHOW MENU =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    //Validate that variables exist on the page
+    if (toggle && nav ) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu');
